7633615d483f56f3946e7703350b7e28bc9a906c
alfiensukma <alfien.sukmaprawira1724@gmail.com>
2023-09-27 09:29:02 +0700

    [Add] Template Controller, [Update] Response

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -5,7 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Template;
 
+
 class TemplateController extends Controller
 {
-    //
+    public function index(){
+        $data = Template::get();
+
+        return response()->json([
+            'data' => $data,
+            'response' => 200
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RateTemplateController;
+use App\Http\Controllers\TemplateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ use App\Http\Controllers\RateTemplateController;
 Route::post('/register', [RegisterController::class,'store']);
 Route::post('/login', [LoginController::class,'login']);
 Route::get('/rating', [RateTemplateController::class,'showRates']);
+Route::get('/main', [TemplateController::class,'index']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
