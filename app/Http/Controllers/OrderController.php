ee4df14b8f74bb823a7835cc23b48fabdec33354
Daniar27 <daniarmiftah@gmail.com>
2023-10-02 22:59:04 +0700

    <fix> fix error handling and bug cover image andattachment file orderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -74,81 +74,95 @@ class OrderController extends Controller
     }
 
     public function store(Request $request, $id){
-
-        if($request->hasfile('cover_image')){
-            $cover = $request->file('cover_image');
-            $coverName = Str::random(10) . "." . $cover->getClientOriginalName();
-            $path = public_path('assets/cover');
-            File::makeDirectory($path, 0777, true, true);
-            $location = 'assets/cover';
-            $cover->move($location, $coverName);
-        } else {
-            $coverName = 'default.png';
-        }
-
-        $eventInformation = EventInformation::create([
-            'bride_name'          => $request->bride_name,
-            'groom_name'          => $request->groom_name,
-            'bride_father_name'   => $request->bride_father_name,
-            'bride_mother_name'   => $request->bride_mother_name,
-            'groom_father_name'   => $request->groom_father_name,
-            'groom_mother_name'   => $request->groom_mother_name,
-            'cover_image'         => $coverName,
-            'date_event'          => $request->date_event,
-            'guests'              => $request->guests,
-            'account_number'      => $request->account_number,
-            'account_holder_name' => $request->account_holder_name,
-            'quotes'              => $request->quotes,
-            'address'             => $request->address,
-            'building_name'       => $request->building_name,
-            'lat'                 => $request->lat,
-            'lng'                 => $request->lng,
-        ]);
-
-        if ($request->has('attachment_name')) {
-            foreach ($request->file('attachment_name') as $attachmentFile) {
-                $fileName = Str::random(10) . '.' . $attachmentFile->getClientOriginalExtension();
-                $path = public_path('assets/attachments');
-                
-                if (!File::exists($path)) {
-                    File::makeDirectory($path, 0777, true, true);
+        try{
+            if($request->hasfile('cover_image')){
+                $cover = $request->file('cover_image');
+                $coverName = Str::random(10) . "." . $cover->getClientOriginalName();
+                $path = public_path('assets/cover');
+                File::makeDirectory($path, 0777, true, true);
+                $location = 'assets/cover';
+                $cover->move($location, $coverName);
+            } else {
+                $coverName = 'default.png';
+            }
+    
+            $eventInformation = EventInformation::create([
+                'bride_name'          => $request->bride_name,
+                'groom_name'          => $request->groom_name,
+                'bride_father_name'   => $request->bride_father_name,
+                'bride_mother_name'   => $request->bride_mother_name,
+                'groom_father_name'   => $request->groom_father_name,
+                'groom_mother_name'   => $request->groom_mother_name,
+                'cover_image'         => $coverName,
+                'date_event'          => $request->date_event,
+                'guests'              => $request->guests,
+                'account_number'      => $request->account_number,
+                'account_holder_name' => $request->account_holder_name,
+                'quotes'              => $request->quotes,
+                'address'             => $request->address,
+                'building_name'       => $request->building_name,
+                'lat'                 => $request->lat,
+                'lng'                 => $request->lng,
+            ]);
+    
+            if ($request->has('attachment_name')) {
+                foreach ($request->file('attachment_name') as $attachmentFile) {
+                    $fileName = Str::random(10) . '.' . $attachmentFile->getClientOriginalExtension();
+                    $path = public_path('assets/attachments');
+                    
+                    if (!File::exists($path)) {
+                        File::makeDirectory($path, 0777, true, true);
+                    }
+                    
+                    $location = 'assets/attachments';
+                    $attachmentFile->move($location, $fileName);
+            
+                    $attachment = new Attachment([
+                        'attachment_name' => $fileName,
+                        'event_information_id' => $eventInformation->id,
+                    ]);
+            
+                    $eventInformation->attachment()->save($attachment);
                 }
-                
-                $location = 'assets/attachments';
-                $attachmentFile->move($location, $fileName);
-        
+            } else {
+                $fileName = 'default.png';
+            
                 $attachment = new Attachment([
                     'attachment_name' => $fileName,
                     'event_information_id' => $eventInformation->id,
                 ]);
-        
-                $eventInformation->attachments()->save($attachment);
+            
+                $eventInformation->attachment()->save($attachment);
             }
-        } else {
-            $fileName = 'default.png';
-        
-            $attachment = new Attachment([
-                'attachment_name' => $fileName,
+    
+           $order = new Order([
+                'order_code'           => Str::random(10),
+                'user_id'              => '2',
+                'template_id'          => $id,
                 'event_information_id' => $eventInformation->id,
+                'order_verification'   => '1',
+           ]);
+           $eventInformation->order()->save($order);
+           
+           return response()->json([
+                    'eventInformation' => $eventInformation,
+                    'message' => 'Data Undangan Pernikahan berhasil dibuat', 
+                    'response' => 200
+                ]);
+        }catch (\Exception $e) {
+            if (isset($coverName) && $coverName !== 'default.png') {
+                $coverPath = public_path('assets/cover/' . $coverName);
+                if (File::exists($coverPath)) {
+                    File::delete($coverPath);
+                }
+            }
+    
+            return response()->json([
+                'message' => 'Gagal membuat data Undangan Pernikahan: ' . $e->getMessage(), 
+                'response' => 500
             ]);
         
-            $eventInformation->attachments()->save($attachment);
         }
-
-       $order = new Order([
-            'order_code'           => Str::random(10),
-            'user_id'              => '2',
-            'template_id'          => $id,
-            'event_information_id' => $eventInformation->id,
-            'order_verification'   => '1',
-       ]);
-       $eventInformation->order()->save($order);
-       
-       return response()->json([
-                'eventInformation' => $eventInformation,
-                'message' => 'Data Undangan Pernikahan berhasil dibuat', 
-                'response' => 200
-            ]);
     }
 }
 
